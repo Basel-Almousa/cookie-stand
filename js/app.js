65a414016fba8260dcad074877020d5911cb1ca7
Basel-Almousa <basel.almuusa@gmail.com>
2021-06-27 23:12:23 +0300

    ,

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict'
+let Seattle= {
+    min: 23,
+    max: 65,
+    avg: 6.3,
+
+getcust= function(max, min) {
+    return (math.floor(math.random() * ( this.max - this.min + 1) + this.min));
+}
+ };
+ console.log(Seattle.getcust());
+ 
+ let tokyo= {
+    min: 3,
+    max: 24,
+    avg: 1.2,
+
+getcust1= function(max, min) {
+    return (math.floor(math.random() * ( this.max - this.min + 1) + this.min));
+}
+ };
+ console.log(tokyo.getcust1());
+
+ let Dubai= {
+    min: 23,
+    max: 65,
+    avg: 6.3,
+
+getcust2= function(max, min) {
+    return (math.floor(math.random() * ( this.max - this.min + 1) + this.min));
+}
+ };
+ console.log(Dubai.getcust2());
+
+ 
+ let Paris= {
+    min: 20,
+    max: 38,
+    avg: 2.3,
+
+getcust3= function(max, min) {
+    return (math.floor(math.random() * ( this.max - this.min + 1) + this.min));
+}
+ };
+ console.log(Paris.getcust3());
+
